5bb12c9877b22aa939c4539c22766a2720c7e497
tatsuoNakano <aaaoauo1118@gmeil.com>
2025-07-10 21:32:02 +0900

    空ファイル追加　とりあえずビルド成功　開発サーバーも元気な状態

---
+++ gatsby/src/pages/maindmap.tsx
@@ -0,0 +1,16 @@
+// src/pages/mindmap.tsx
+import React from "react"
+import Layout from "../components/Common/Layout"
+
+const MindMapPage: React.FC = () => {
+    return (
+        <Layout>
+            <div className="p-4 text-light bg-black min-vh-100">
+                <h1 className="text-info">Mindmap</h1>
+                {/* ここにマインドマップ機能を追加していきます */}
+            </div>
+        </Layout>
+    )
+}
+
+export default MindMapPage
